d21b7c309466721fe96da06e64668149ae7211df
LaxmanKharabe <laxman31097@gmail.com>
2024-06-07 16:53:45 +0530

    removing duplicates

--- js1.js
+++ js1.js
@@ -1,7 +1,9 @@
 const arr = [10, 20,30, 40, 10, 20];
 
 const removeDuplicates = (arr) =>{
-    return [...new Set(arr)];
-}
+    // return [...new Set(arr)];
 
+    const a = arr.filter((ele, ind) => arr.indexOf(ele) === ind);
+    return a
+}
 console.log(removeDuplicates(arr));
